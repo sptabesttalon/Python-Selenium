e2f4b1c915fdc555efec1a0cad527ec0c98e6f16
sptabesttalon <87066568+sptabesttalon@users.noreply.github.com>
2021-07-27 20:04:30 +0700

    Update reg2.py

--- reg2.py
+++ reg2.py
@@ -5,31 +5,33 @@ import selenium
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(executable_path="C:/Code/Chromedriver.exe")
+driver = webdriver.Chrome(executable_path="C:/Python/Chromedriver.exe")
 driver.maximize_window ()
 
 driver.get('https://facebook.com/reg')
-time.sleep(3) #ok
+time.sleep(5) #ok
 elem = driver.find_element_by_name("lastname")
-elem.send_keys("Nea ")
-time.sleep(3)
-elem = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div/div/div[1]/form/div[1]/div[1]/div[1]/div[2]/div/div[1]/input")
-elem.send_keys("Nopro")
-time.sleep(1) #ok
-elem = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div/div/div[1]/form/div[1]/div[5]/div[2]/span/span/select[3]").click()
-time.sleep(1) #năm
+elem.send_keys("Katori")
+time.sleep(5)
+elem = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/div[2]/div/div/div[1]/form/div[1]/div[1]/div[1]/div[2]/div/div[1]/input")
+elem.send_keys("Thoeo")
+time.sleep(5) #ok
+elem = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/div[2]/div/div/div[1]/form/div[1]/div[5]/div[2]/span/span/select[3]").click()
+time.sleep(5) #năm
 webdriver.ActionChains(driver).key_down(Keys.ARROW_DOWN).send_keys("2000").perform()
-time.sleep(1)
-elem = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div/div/div[1]/form/div[1]/div[2]/div/div[1]/input")
-elem.send_keys("neanopro1712@stempmail.com")
-time.sleep(1)
-elem = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div/div/div[1]/form/div[1]/div[3]/div/div/div[1]/input")
-elem.send_keys("neanopro1712@stempmail.com")
-time.sleep(1)
-elem = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div/div/div[1]/form/div[1]/div[7]/span/span[2]/input").click()
-time.sleep(1) #giới tính nam
-elem = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div/div/div[1]/form/div[1]/div[4]/div/div[1]/input")
+time.sleep(5)
+elem = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/div[2]/div/div/div[1]/form/div[1]/div[2]/div/div[1]/input")
+elem.send_keys("katorithoeo1@stempmail.com")
+time.sleep(5)
+elem = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/div[2]/div/div/div[1]/form/div[1]/div[3]/div/div/div[1]/input")
+elem.send_keys("katorithoeo1@stempmail.com")
+time.sleep(5)
+elem = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/div[2]/div/div/div[1]/form/div[1]/div[7]/span/span[2]/input").click()
+time.sleep(5) #giới tính nam
+elem = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/div[2]/div/div/div[1]/form/div[1]/div[4]/div/div[1]/input")
 elem.send_keys("haianh251007")
-time.sleep(1)
-elem = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div/div/div[1]/form/div[1]/div[10]/button").click() #đang kí
-time.sleep(1)
+time.sleep(5)
+elem = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/div[2]/div/div/div[1]/form/div[1]/div[10]/button").click() #đang kí
+time.sleep(360)
+driver.get('https://www.facebook.com/profile.php?id=100071156294459&sk=friends')
+time.sleep(5)
